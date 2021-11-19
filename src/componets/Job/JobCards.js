c87d1641500084f11c692bb30605a49a6c41eac9
hammer200911 <jdaniel231@gmail.com>
2021-11-19 17:12:52 -0300

    criado conteudo layout

---
+++ src/componets/Job/JobCards.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Box, Grid, Typography, Button, makeStyles } from "@material-ui/core";
+
+const skills = ["Javascript", "React.js", "Node.js"]
+
+const useStyles = makeStyles((theme) =>({
+  wrapper: {
+    border: '1px solid #e8e8e8',
+
+    "&:hover": {
+      boxShadow: '0px 5px 25px rgba(0, 0, 0, 0.1)', 
+      borderLeft: '6px solid #4d64e4'
+    }
+  },
+  companyName:{ 
+    fontsize: "13.5px",
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(0.75),
+    borderRadius: "5px",
+    display: "inline-block",
+    fontWeight: 600,
+  },
+  skillChip:{ 
+    margin: theme.spacing(0.5),
+    padding: theme.spacing(0.75),
+    fontsize: "14.5px",
+    borderRadius: "5px",
+    transition: ".3s",
+    cursor: "pointer",
+    fontWeight: 600,
+    backgroundColor: theme.palette.secondary.main,
+    color: "#fff"
+  }
+}))
+
+export default (props) => {
+  const classes = useStyles()
+  return(
+    <Box p={2} className={classes.wrapper}>
+      <Grid container alignItems="center">
+        <Grid item xs>
+          <Typography  variant="subtitle1">Frontend</Typography>
+          <Typography className={classes.companyName} variant="subtitle1">Google</Typography>
+        </Grid>
+        <Grid  container item xs>
+          {skills.map(skill => <Grid className={classes.skillChip} item key={skill}>{skill}</Grid>)}
+        </Grid>
+        <Grid item container xs direction="column" alignItems="flex-end">
+          <Grid item>
+            <Typography variant="caption">
+              2577 min ago | Full-Time
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Box mt={2}>
+              <Button variant="outlined">Check</Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
